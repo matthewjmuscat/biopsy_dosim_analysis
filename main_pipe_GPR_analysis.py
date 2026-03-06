bf6e187ec568537be6f51c33c45ccf072ebf0c4b
Matthew Muscat <matthew.muscat@mail.mcgill.ca>
2026-03-06 00:54:19 -0800

    minor

--- main_pipe_GPR_analysis.py
+++ main_pipe_GPR_analysis.py
@@ -58,7 +58,7 @@ def main():
     run_cohort_plots = True
 
     # GP methodology options
-    gp_mean_mode = "ordinary"  # IMPORTANT: Can be "ordinary" or "zero", affects GP mean selection (kriging style simple vs ordinary kriging). It should be ordinary, we have tested for this and zero displays artificially pushed down regression profiles
+    gp_mean_mode = "ordinary"  # IMPORTANT: Can be "ordinary" or "zero", affects GP mean selection (kriging style simple vs ordinary kriging). It should be "ordinary", we have tested for this and zero displays artificially pushed down regression profiles
     semivariogram_method = "shift"  # options: "shift" (legacy), "pairwise" (gap-safe, upgraded allows for proper calculation for Leave One Segment Out pathway)
     run_semivariogram_method_parity_check = True
     semivariogram_pairwise_position_mode = "begin"
